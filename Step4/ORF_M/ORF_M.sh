09015088e99e41f4b37e36c20d8f513c5452c37e
Clark1016 <774099626@qq.com>
2024-08-12 13:41:10 +0800

    Step1 Step2 Step3 Step4 R_Python_script/

---
+++ Step4/ORF_M/ORF_M.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p rack,rack2     # 投递任务的队列 fat，rack，rack2, d2
+#SBATCH --mem=100G # 所需的内存大小
+#SBATCH -N 1       # 1个节点，一般不需要修改
+#SBATCH -n 20      # 所需的CPU的数量
+#SBATCH -e iqtree.sh.e%j # 错误输出
+#SBATCH -o iqtree.sh.o%j # 标准输出
+source /share/home/zhenghanze/miniconda3/etc/profile.d/conda.sh
+conda activate /share/home/zhenghanze/miniconda3/envs/iqtree
+mafft --auto merged_ORF_M.fasta > merged_ORF_M.mafft.fasta
+trimal -in merged_ORF_M.mafft.fasta -out merged_ORF_M.mafft.trimal.fasta -automated1
+iqtree -s merged_ORF_M.mafft.trimal.fasta -T auto -m MFP -b 1000 -redo
+
---
+++ Step4/ORF_N/ORF_N.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p rack,rack2     # 投递任务的队列 fat，rack，rack2, d2
+#SBATCH --mem=100G # 所需的内存大小
+#SBATCH -N 1       # 1个节点，一般不需要修改
+#SBATCH -n 20      # 所需的CPU的数量
+#SBATCH -e iqtree.sh.e%j # 错误输出
+#SBATCH -o iqtree.sh.o%j # 标准输出
+source /share/home/zhenghanze/miniconda3/etc/profile.d/conda.sh
+conda activate /share/home/zhenghanze/miniconda3/envs/iqtree
+mafft --auto merged_ORF_N.fasta > merged_ORF_N.mafft.fasta
+trimal -in merged_ORF_N.mafft.fasta -out merged_ORF_N.mafft.trimal.fasta -automated1
+iqtree -s merged_ORF_N.mafft.trimal.fasta -T auto -m MFP -b 1000 -redo
